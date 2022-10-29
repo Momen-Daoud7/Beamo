4debf84346c29f4a92b183b70d1ffc268077208f
=Momen Daoud <=momendaoud07@gamil.com>
2022-10-28 17:11:42 -0700

    Add user to localStorage

--- src/store/reducers/users.js
+++ src/store/reducers/users.js
@@ -4,15 +4,27 @@ import { auth, provider } from "../../firebase/firebase";
 export const login = createAsyncThunk("users/logins", async () => {
   try {
     const results = await auth.signInWithPopup(provider);
+    // store user in local storage
+    localStorage.setItem(
+      "user",
+      JSON.stringify({
+        ...results.user.multiFactor.user,
+      })
+    );
     return results;
   } catch (error) {
     console.log(error);
   }
 });
 
+// init userToken value from localStorage
+const initUser = localStorage.getItem("user")
+  ? JSON.parse(localStorage.getItem("user"))
+  : null;
+
 const userSlice = createSlice({
   name: "users",
-  initialState: { user: false },
+  initialState: { user: initUser },
   extraReducers: {
     [login.fulfilled]: (state, action) => {
       state.user = action.payload.user.multiFactor.user;
